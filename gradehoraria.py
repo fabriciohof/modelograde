e45d70e707e3716470e93123c79c65ad85add01d
Fabrício Formentini <91196904+fabriciohof@users.noreply.github.com>
2022-08-11 22:16:19 -0300

    Update gradehoraria.py

--- gradehoraria.py
+++ gradehoraria.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[13]:
 
 
 import pandas as pd
 
-grade = {'Segunda':['OC1','OC1','OC1','INTERVALO -','MAT','MAT','MAT','//'],'Terça':['MAT','MAT','//','INTERVALO -','RD1','RD1','RD1','RD1'],
-'Quarta':['ICG','ICG','ICG','INTERVALO -','//','LEI','LEI','//'],'Quinta':['//','CEL','CEL','INTERVALO -','RD1','//','ICG','ICG'],
-'Sexta':['//','//','//','INTERVALO','//','OC1','OC1','OC1']}
+grade = {'Segunda':['OC1','OC1','OC1','INTERVALO -','MAT','MAT','MAT','//'],
+         'Terça':['MAT','MAT','//','INTERVALO -','RD1','RD1','RD1','RD1'],
+         'Quarta':['ICG','ICG','ICG','INTERVALO -','//','LEI','LEI','//'],
+         'Quinta':['//','CEL','CEL','INTERVALO -','RD1','//','ICG','ICG'],
+         'Sexta':['//','//','//','INTERVALO','//','OC1','OC1','OC1']}
 
 g  = pd.DataFrame(data=grade)
 df = pd.DataFrame(grade, index = ["7:00-7:45","7:45-8:30","8:30-9:15","9:15-9:30","9:30-10:15","10:15-11:00","11:00-11:45","11:45-12:30"])
